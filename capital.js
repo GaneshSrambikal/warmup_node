4e9c8092b34c2c9f6dbdacac921a310931ea9789
unknown <ganesh.srambikal@gmail.com>
2019-01-17 18:07:59 +0530

    capitalize

---
+++ capital.js
@@ -0,0 +1,7 @@
+let word = 'ForEverjavaScript',
+    //length = word.length,
+    caps =  word.slice(0, 1).toUpperCase(),
+    right = word.slice(1).toLowerCase()
+
+    console.log(caps+right)
+    
