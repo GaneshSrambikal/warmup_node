dadb26bc923fc38a4ea53c18cf870fb7d5755e67
unknown <ganesh.srambikal@gmail.com>
2019-01-18 21:35:47 +0530

    capitalize

--- capital.js
+++ capital.js
@@ -1,6 +1,27 @@
-let word = 'ForEverjavaScript',
-    caps =  word.slice(0, 1).toUpperCase(),
-    right = word.slice(1).toLowerCase()
 
-    console.log(caps+right)
-    
+// const capitalizeMe = (word) => {
+//     let 
+//         caps = word.slice(0, 1).toUpperCase(),
+//         right = word.slice(1).toLowerCase()
+
+//     console.log(caps + right) //Foreverjavascript
+// }
+// capitalizeMe('forEverJavaScript')
+
+
+
+
+function capitalizeMe(x) {
+    var word = x,
+        length = word.length,
+        firstChar = word.charAt(0).toUpperCase(),
+        restChar = '';
+        
+    for (var i = 1; i <= length - 1; i++) {
+        restChar += word[i].toLowerCase();
+    }
+
+    console.log(firstChar + restChar) //Foreverjavascript
+}
+
+capitalizeMe('forEverJavaScript')
