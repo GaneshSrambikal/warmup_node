7f07d347c09094d08db246e49eaa4c0a4bf1ddc3
unknown <ganesh.srambikal@gmail.com>
2019-01-23 19:02:17 +0530

    bufferd

--- buffers.js
+++ buffers.js
@@ -7,13 +7,13 @@
  * */
 //Create buffer
 //Method 1
-//var buffer = new buffer(10);//creates an unintiated buffer of 10 octets
+var buffer = new buffer(10);//creates an unintiated buffer of 10 octets
 
 //Method 2
-//var buffer = new Buffer([10, 20, 45, 78]);//create and buffer from given array
+var buffer = new Buffer([10, 20, 45, 78]);//create and buffer from given array
 
 //Method 3
-//var budder = new Buffer("HI there!,buffer", "utf-8");//with optional encoding
+var budder = new Buffer("HI there!,buffer", "utf-8");//with optional encoding
 
 //Writing to Buffer
 /** buf.write(string[,offset][,length][,encoding]) 
@@ -45,9 +45,50 @@ console.log("wrtten data: " + data);
  */
 //example
 buf = new Buffer.alloc(26);
-for(let i =0 ;i<26;i++){
-    buf[i]=i+97;
+for (let i = 0; i < 26; i++) {
+    buf[i] = i + 97;
 }
-console.log(buf.toString('ascii'));
+console.log(buf.toString('ascii', 0, 7));
 
+//Convert buffer to Json
+/**buf.toJSON(buf)
+ * returns a JSON representation of buffer instances
+ */
+//Example
+let buf = new Buffer.from('hello there'),
+    json = buf.toJSON(buf);
+console.log(json);
+
+/** Concatenate Buffers
+ * buffer.concat([list][totallength])
+ * methods returns buffer instance
+ */
+//Example
+let buffer1 = new Buffer.from('Hi there,');
+let buffer2 = new Buffer.from('im learning NodeJs.');
+let buffer3 = Buffer.concat([buffer1, buffer2]);
+
+// console.log(buffer3.toString());
+
+/**Compare a buffer
+ * buffer.compare(otherbuffer)
+ * return a number which indicates wheather it comes before or after or the same.
+ */
+var buffer1 = new Buffer('BC');
+var buffer2 = new Buffer('aBC');
+var result = buffer1.compare(buffer2);
+console.log(result)
+if (result < 0) {
+    console.log(buffer1 + " comes before " + buffer2);
+} else if (result === 0) {
+    console.log(buffer1 + " is same as " + buffer2);
+} else {
+    console.log(buffer1 + " comes after " + buffer2);
+}
+
+//Copy buffer
+/**buffer.copy(otherbuffer)
+ * copies data from target buffer to the destination 
+ * 
+ */
 
