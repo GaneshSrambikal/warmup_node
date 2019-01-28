ec6cf3ff7ee66c79bf57c7ae498d8bdca297e59a
unknown <ganesh.srambikal@gmail.com>
2019-01-28 16:33:09 +0530

    path modules

---
+++ utility_modules/path_modules.js
@@ -0,0 +1,19 @@
+/** Nodejs Path Modules
+ * path module is used to handle and transform file paths.
+ * To import path modules use syntax
+ * let path = require('path');
+ */
+
+let path = require('path');
+
+//path.normalize - normalizes the paths(for ex. '//','..')
+console.log(path.normalize("Desktop\\warmup_node\\buffers.js"));
+
+//path.resolve - returns the whole fil paths
+console.log(path.resolve('buffers.js'));
+ 
+//returns the extension of the specified filename
+console.log(path.extname('buffers.js'));
+
+//path.join(path1,path2) - joins the paths
+console.log(path.join('\Desktop\warmup_node','utility_modules'));
