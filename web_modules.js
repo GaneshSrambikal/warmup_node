7a27eee267c9ca8af1a039add87567bc988c5abf
unknown <ganesh.srambikal@gmail.com>
2019-02-01 20:07:26 +0530

    web server

---
+++ web_modules.js
@@ -0,0 +1,16 @@
+/** Web Modules
+ * Whats a web Server?
+ * A web Server is a software application which handles http request send by http client.
+ * It returns web pages as a response to the client.
+ * Most of the web server uses scripting language to redirect the task to an application server
+ * which retrieves data from the database and performs complex logic and then
+ * send it to http client through web server.
+ * 
+ * web application archictecture consists of 4 layers-
+ * A web archictecture consist of :-
+ * Client - its basically the browsers or applications from different devices
+ * web server - takes the hhtp client request and response by web pages.
+ * bussiness layer - it consists of 
+ * Data layer -
+ * 
+ */
